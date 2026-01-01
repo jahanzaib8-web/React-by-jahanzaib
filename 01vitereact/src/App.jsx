29cfa47f75ed98f0c25659c7c135964476f7c009
Jahanzaib Ahmad <jahanzaib@gmail.com>
2026-01-01 16:56:55 +0500

    React project

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,15 @@
+import React from "react";
 
 function App() {
-  
   return (
-    
-    <h1>Chai aur react with vite |Jahanzaib ahmad</h1>
-  )
+    <div>
+      <h1>My React App</h1>
+
+      <a href="https://google.com" target="_blank">
+        Visit Google
+      </a>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,9 @@
+function Chai() {
+  return (
+    <a href="https://google.com" target="_blank">
+      Visit Google
+    </a>
+  );
+}
+
+export default Chai;
