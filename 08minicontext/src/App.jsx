fcf4fd51186f8d1994d877a24b66daad75af2940
Jahanzaib Ahmad <jahanzaib@gmail.com>
2026-02-05 23:02:13 +0500

    context api

--- 08minicontext/src/App.jsx
+++ 08minicontext/src/App.jsx
@@ -1,9 +1,12 @@
+
 import './App.css'
-import Login from './components1/Login'
-import Profile from './components1/Profile'
+import Login from './components/Login'
+import Profile from './components/Profile'
 import UserContextProvider from './context/UserContextProvider'
 
 function App() {
+  
+
   return (
     <UserContextProvider>
       <h1>React with Chai and share is important</h1>
